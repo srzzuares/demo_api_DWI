83f43ec61a627f33353e4beec465c0ee2dcedc0b
sr <srzzuares@gmail.com>
2023-05-29 19:09:25 -0600

    6 New Buttons and code in activity_main.xml and MainActivity.java for backend

--- app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
+++ app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
@@ -3,12 +3,22 @@ package mx.edu.utxj.dwi.demo_api;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSave.findViewById(R.id.btnSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
