e9dfe8fdcedc66f33e760eff6c790d1117cddda5
sr <srzzuares@gmail.com>
2023-06-06 16:03:59 -0600

    fixwed06

--- app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
+++ app/src/main/java/mx/edu/utxj/dwi/demo_api/MainActivity.java
@@ -1,5 +1,32 @@
 package mx.edu.utxj.dwi.demo_api;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -15,26 +42,28 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private String url = "http://10.10.62.17:3300/";
-    private Button btnSave, btnDelete, btnUpdate, btnSearch;
+    private Button btnSave;
+    private Button btnSearch;
+    private Button btnDelete;
+    private Button btnUpdate;
     private EditText etCodigoBarras,etDescripcion,etMarca,etprecioCompra,etprecioVenta,etExistencias;
     private ListView lvProducts;
     private RequestQueue requestQueue;
     private JsonArrayRequest jsonArrayRequest;
     private ArrayList<String> origenDatos= new ArrayList<String>();
     private ArrayAdapter<String> adapter;
+    private String url = "http://10.10.62.17:3300/";
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         lvProducts=findViewById(R.id.lvProducts);
         listProducts();
 
+        // --------------------------------------------------------------- buscar
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,48 +118,154 @@ public class MainActivity extends AppCompatActivity {
                 requestQueue.add(peticion);
             }
         });
+
+// --------------------------------------------------------------- guardar
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                JSONObject insertarDAtos = new JSONObject();
+                JSONObject producto= new JSONObject();
                 try {
-                    insertarDAtos.put("codigobarras",etCodigoBarras.getText().toString());
-                    insertarDAtos.put("descripcion",etDescripcion.getText().toString());
-                    insertarDAtos.put("marca",etMarca.getText().toString());
-                    insertarDAtos.put("preciocompra",Integer.parseInt(etprecioCompra.getText().toString()));
-                    insertarDAtos.put("precioventa",Integer.parseInt(etprecioVenta.getText().toString()));
-                    insertarDAtos.put("existencias",Integer.parseInt(etExistencias.getText().toString()));
+                    producto.put("codigobarras",etCodigoBarras.getText().toString());
+                    producto.put("descripcion",etDescripcion.getText().toString());
+                    producto.put("marca",etMarca.getText().toString());
+                    producto.put("preciocompra",etprecioCompra.getText().toString());
+                    producto.put("precioventa",etprecioVenta.getText().toString());
+                    producto.put("existencias",etExistencias.getText().toString());
                 }catch (JSONException e){
-                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                 }
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(
                         Request.Method.POST,
-                        url+"insert/",
-                        insertarDAtos,
+                        url + "insert/" ,
+                        producto,
                         new Response.Listener<JSONObject>() {
                             @Override
                             public void onResponse(JSONObject response) {
                                 try {
-                                    if (response.getString("status").equals("Producto Insertado"))
-                                        Toast.makeText(MainActivity.this,"Producto insertado con éxito", Toast.LENGTH_SHORT).show();
+                                    if (response.getString("status").equals("Producto insertado"))
+                                        Toast.makeText(MainActivity.this, "¡Producto insertado con exito", Toast.LENGTH_SHORT).show();
+                                    etCodigoBarras.setText("");
+                                    etDescripcion.setText("");
+                                    etMarca.setText("");
+                                    etprecioCompra.setText("");
+                                    etprecioVenta.setText("");
+                                    etExistencias.setText("");
                                     listProducts();
-                                }catch (JSONException e){
-                                    Toast.makeText(MainActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                                } catch (JSONException e){
+                                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                            }
+                        }
+                );
+                requestQueue.add(jsonObjectRequest);
+            }});
+
+
+// --------------------------------------------------------------- actualizar
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JSONObject updatedProduct = new JSONObject();
+                try {
+                    updatedProduct.put("codigobarras", etCodigoBarras.getText().toString());
+                    updatedProduct.put("descripcion", etDescripcion.getText().toString());
+                    updatedProduct.put("marca", etMarca.getText().toString());
+                    updatedProduct.put("preciocompra", etprecioCompra.getText().toString());
+                    updatedProduct.put("precioventa", etprecioVenta.getText().toString());
+                    updatedProduct.put("existencias", etExistencias.getText().toString());
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+                JsonObjectRequest updateRequest = new JsonObjectRequest(
+                        Request.Method.PUT,
+                        url + "actualizar/" + etCodigoBarras.getText().toString(),
+                        updatedProduct,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    if (response.getString("status").equals("Producto actualizado")) Toast.makeText(MainActivity.this, "Producto actualizado exitosamente", Toast.LENGTH_SHORT).show();
+                                    etCodigoBarras.setText("");
+                                    etDescripcion.setText("");
+                                    etMarca.setText("");
+                                    etprecioCompra.setText("");
+                                    etprecioVenta.setText("");
+                                    etExistencias.setText("");
+                                    listProducts();
+                                } catch (JSONException e) {
+                                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
                                 }
                             }
                         },
                         new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_SHORT).show();
+                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
                             }
                         }
                 );
-
-                requestQueue.add(jsonObjectRequest);
+                requestQueue.add(updateRequest);
             }
         });
+
+
+// --------------------------------------------------------------- eliminar
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String codigoBarras = etCodigoBarras.getText().toString();
+
+                JsonObjectRequest deleteRequest = new JsonObjectRequest(
+                        Request.Method.DELETE,
+                        url + "borrar/" + codigoBarras,
+                        null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    if (response.getString("status").equals("Producto eliminado")) {
+                                        Toast.makeText(MainActivity.this, "No se pudo eliminar el producto", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Toast.makeText(MainActivity.this, "Producto eliminado exitosamente", Toast.LENGTH_SHORT).show();
+                                        listProducts();
+                                    }
+                                } catch (JSONException e) {
+                                    Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                                etCodigoBarras.setText("");
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                );
+                requestQueue.add(deleteRequest);
+
+            }
+        });listProducts();
+
+
+
+
+
+
+
+
+
     }
+
+
 
     protected void listProducts(){
 
@@ -143,12 +279,11 @@ public class MainActivity extends AppCompatActivity {
                         //Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
                         for(int i = 0;i < response.length(); i++){
                             try {
-                                String codigobarras = response.getJSONObject(i).getString("codigobarras");
+                                String codigobarras =response.getJSONObject(i).getString("codigobarras");
                                 String descripcion = response.getJSONObject(i).getString("descripcion") ;
                                 String marca = response.getJSONObject(i).getString("marca");
-                                origenDatos.add(codigobarras+">>"+descripcion+"::"+marca);
+                                origenDatos.add(codigobarras +"::" +descripcion+"::"+marca);
                             } catch (JSONException e) {
-                                throw new RuntimeException(e);
                             }
                         }
                         adapter = new ArrayAdapter<>(MainActivity.this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, origenDatos);
